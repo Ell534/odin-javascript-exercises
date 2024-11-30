1132ec39fae83323fd8a351651743ca43b292502
Ell534 <elliot94b@gmail.com>
2024-11-30 17:29:41 +0000

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  let sumVal = 0
 
+  if (min < 0 || max < 0) {
+    return "ERROR"
+  }
+
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR"
+  }
+
+  if (min > max) {
+    [min, max] = [max, min]
+  }
+
+  for (let i = min; i <= max; i++) {
+    sumVal += i
+  }
+  return sumVal
 };
 
 // Do not edit below this line
