428f024d72049f2977e97a3d6ab8d8843d918bc0
Ell534 <elliot94b@gmail.com>
2024-11-30 16:35:45 +0000

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(input, repeats) {
+  if (repeats < 0) {
+    return "ERROR"
+  }
+  let returnString = ""
+  for (let i = 0; i < repeats; i++) {
+    returnString += input
+  }
+  return returnString
 };
 
 // Do not edit below this line
