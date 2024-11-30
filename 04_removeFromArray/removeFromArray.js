07d489878f42742ea84a2491a56c1e9a11c726bb
Ell534 <elliot94b@gmail.com>
2024-11-30 17:12:05 +0000

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+  const outputArray = []
+
+  inputArray.forEach((item) => {
+    if (!args.includes(item)) {
+      outputArray.push(item)
+    }
+  })
+  return outputArray
 };
 
 // Do not edit below this line
