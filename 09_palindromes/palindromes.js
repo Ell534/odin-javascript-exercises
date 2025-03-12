5697b549b40606d067a4b34d42b8613b9eadfa3d
Ell534 <elliot94b@gmail.com>
2025-03-12 19:10:28 +0000

    complete exercises 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function(word) {
+  const alphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+  const cleanedWord = word.toLowerCase().split("").filter((char) => alphaNumeric.includes(char)).join("");
+
+  const reversedWord = cleanedWord.split("").reverse().join("");
+  return cleanedWord === reversedWord;
 };
 
 // Do not edit below this line
